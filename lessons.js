e516f04e2c16ab480116bde0f7114bca8ff97dca
Mohamed Abid Miah <mm3852@live.mdx.ac.uk>
2023-11-11 23:47:01 +0000

    added 10 lessons and changed values

--- lessons.js
+++ lessons.js
@@ -1,6 +1,14 @@
 const lessions = [
   { subject: "Maths", location: "Manchester", price: 40, Aval: 5 },
-  { subject: "English", location: "London", price: 20, Aval: 5 },
+  { subject: "English", location: "London", price: 40, Aval: 5 },
+  { subject: "Science", location: "Bristol", price: 40, Aval: 5 },
+  { subject: "Geography", location: "Bristol", price: 20, Aval: 5 },
+  { subject: "Arts", location: "Lodnon", price: 80, Aval: 5 },
+  { subject: "Dance", location: "London", price: 160, Aval: 5 },
+  { subject: "Music", location: "Cardiff", price: 80, Aval: 5 },
+  { subject: "Computer Science", location: "Edinburgh", price: 180, Aval: 5 },
+  { subject: "Drama", location: "Dublin", price: 60, Aval: 5 },
+  { subject: "DT", location: "Birmingham", price: 100, Aval: 5 },
 ];
 
 export default lessions;
