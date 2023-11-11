80b738bb94ec9789175199a533f2442a92136687
Mohamed Abid Miah <mm3852@live.mdx.ac.uk>
2023-11-11 01:54:27 +0000

    made a lessons object stored in seperate file

---
+++ lessons.js
@@ -0,0 +1,6 @@
+const lessons = [
+  { subject: "Maths", location: "London", price: "£20", Aval: 5 },
+  { subject: "English", location: "London", price: "£20", Aval: 5 },
+];
+
+export default lessons; 
