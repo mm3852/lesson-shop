b571806dcfc7b969f78be44ecfc837af2b679529
Mohamed Abid Miah <mm3852@live.mdx.ac.uk>
2023-11-11 20:44:31 +0000

    changed some of the values

--- lessons.js
+++ lessons.js
@@ -1,6 +1,6 @@
 const lessions = [
-  { subject: "Maths", location: "London", price: "£20", Aval: 5 },
-  { subject: "English", location: "London", price: "£20", Aval: 5 },
+  { subject: "Maths", location: "Manchester", price: 20, Aval: 5 },
+  { subject: "English", location: "London", price: 20, Aval: 5 },
 ];
 
 export default lessions;
