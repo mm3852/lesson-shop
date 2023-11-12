5a730cba627e02425a838afbb211878ca44a4f63
Mohamed Abid Miah <mm3852@live.mdx.ac.uk>
2023-11-12 01:34:17 +0000

    added images

--- lessons.js
+++ lessons.js
@@ -1,14 +1,14 @@
 const lessions = [
-  { subject: "Maths", location: "Manchester", price: 40, Aval: 5 },
-  { subject: "English", location: "London", price: 40, Aval: 5 },
-  { subject: "Science", location: "Bristol", price: 40, Aval: 5 },
-  { subject: "Geography", location: "Bristol", price: 20, Aval: 5 },
-  { subject: "Arts", location: "Lodnon", price: 80, Aval: 5 },
-  { subject: "Dance", location: "London", price: 160, Aval: 5 },
-  { subject: "Music", location: "Cardiff", price: 80, Aval: 5 },
-  { subject: "Computer Science", location: "Edinburgh", price: 180, Aval: 5 },
-  { subject: "Drama", location: "Dublin", price: 60, Aval: 5 },
-  { subject: "DT", location: "Birmingham", price: 100, Aval: 5 },
+  { subject: "Maths", location: "Manchester", price: 40, icon: "fa fa-percent" ,Aval: 5 },
+  { subject: "English", location: "London", price: 40, icon: "fa fa-pencil", Aval: 5 },
+  { subject: "Science", location: "Bristol", price: 40, icon: "	fa fa-flask", Aval: 5 },
+  { subject: "Geography", location: "Bristol", price: 20, icon: "	fa fa-map",  Aval: 5 },
+  { subject: "Arts", location: "Lodnon", price: 80, icon: "	fa fa-eyedropper", Aval: 5 },
+  { subject: "Dance", location: "London", price: 160, icon: "fa fa-street-view", Aval: 5 },
+  { subject: "Music", location: "Cardiff", price: 80, icon: "fa fa-music", Aval: 5 },
+  { subject: "ICT", location: "Edinburgh", price: 180, icon: "fa fa-laptop", Aval: 5 },
+  { subject: "Drama", location: "Dublin", price: 60, icon: "	fa fa-video-camera",Aval: 5 },
+  { subject: "DT", location: "Birmingham", price: 100, icon: "	fa fa-gear", Aval: 5 },
 ];
 
 export default lessions;
