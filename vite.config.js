fe9a6d9f81209902b22953cfe472398b1e8553db
Dr <Dr>
2024-03-10 18:39:38 +0000

    gihub page

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,8 @@
-import { fileURLToPath, URL } from "node:url";
-
 import vue from "@vitejs/plugin-vue";
+import { fileURLToPath, URL } from "node:url";
 import { defineConfig } from "vite";
 import { VitePWA } from "vite-plugin-pwa";
 
-module.exports = {
-  publicPath: process.env.NODE_ENV === "production" ? "/lesson-shop/" : "/",
-};
-
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), VitePWA({ registerType: "autoUpdate" })],
   resolve: {
@@ -16,4 +10,5 @@ export default defineConfig({
       "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
+  base: "/lesson-shop/",
 });
