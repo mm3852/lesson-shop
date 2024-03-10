50fee068b98b56abd59b77bef46d861f755a9409
Dr <Dr>
2024-03-10 18:32:45 +0000

    github page

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,10 @@ import vue from "@vitejs/plugin-vue";
 import { defineConfig } from "vite";
 import { VitePWA } from "vite-plugin-pwa";
 
+module.exports = {
+  publicPath: process.env.NODE_ENV === "production" ? "/lesson-shop/" : "/",
+};
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), VitePWA({ registerType: "autoUpdate" })],
