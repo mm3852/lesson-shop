2b04588089bbb203c4c40e03c77c45f4642b1c7f
Dr <Dr>
2024-04-11 04:54:55 +0100

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/lesson-shop/sw.js', { scope: '/lesson-shop/' })})}
