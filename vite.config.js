04c47c9912f1597eaf2076175653d2a3406f057a
Dr <Dr>
2024-04-11 04:50:38 +0100

    pwa fix

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,27 @@ import { defineConfig } from "vite";
 import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
-  plugins: [vue(), VitePWA({ registerType: "autoUpdate" })],
+  plugins: [
+    vue(),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "/public.favicon.ico",
+            size: "512x512",
+            type: "image/ico",
+            purpose: "any maskable",
+          },
+        ],
+      },
+    }),
+  ],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
-  base: "/lesson-shop/",
+  base: "/",
 });
